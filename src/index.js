5e8b249d8ebb02b267d1fd82e8f29757920a4bc3
Jaime <jamesreuss1@gmail.com>
2021-03-15 15:53:29 -0700

    dealt with merge conflicts

--- src/index.js
+++ src/index.js
@@ -5,16 +5,9 @@ import App from "./components/App.jsx";
 import playGame from "./phaser/scene";
 import scene2 from "./phaser/scene2";
 import LVL1 from "./phaser/scene_lvl1";
-<<<<<<< HEAD
 import LVL2 from "./phaser/scene_lvl2"
 import Preloader from "./phaser/helper/preloader";
 import Example from "./phaser/flickeringSpotlightEx";
-=======
-import Preloader from "./phaser/helper/preloader";
-
-
-//console.log(App);
->>>>>>> emitter
 
 console.log("does this show up?")
 export const config = {
@@ -30,11 +23,7 @@ export const config = {
     }
   },
 
-<<<<<<< HEAD
   scene: [Preloader, LVL1, LVL2, playGame, scene2],
-=======
-  scene: [Preloader, LVL1, playGame, scene2],
->>>>>>> emitter
   scale: {
     zoom: 1.5
   }
