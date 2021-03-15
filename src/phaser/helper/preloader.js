139cea5b56e81a1db1493702f62919bab9f16958
Jaime <jamesreuss1@gmail.com>
2021-03-15 16:23:18 -0700

    fixed the merge conflictsgit add --all

--- src/phaser/helper/preloader.js
+++ src/phaser/helper/preloader.js
@@ -1,19 +1,11 @@
 import Phaser from "phaser";
 import NeoImg from "../../assets/Neo.png";
-<<<<<<< HEAD
 import caveTiles from "../../assets/tiles/mainlev_build.png";
 import LVL1 from "../../assets/tiles/lvl1.json"
 import LVL2 from "../../assets/tiles/lvl2.json"
 import BG1 from "../../assets/backgrounds/background3.png"
 const gameState = {};
-=======
 import mask from "../../assets/mask1.png"
-import caveTiles from "../../assets/tiles/mainlev_build.png";
-import LVL1 from "../../assets/tiles/lvl1.json"
-const gameState = {};
-import BG1 from "../../assets/backgrounds/background3.png"
->>>>>>> emitter
-// import Scene_LVL1 from "./phaser/scene_lvl1"
 
 export default class Preloader extends Phaser.Scene {
   constructor() {
@@ -21,24 +13,15 @@ export default class Preloader extends Phaser.Scene {
   }
   
   preload() {
-<<<<<<< HEAD
-    
-=======
->>>>>>> emitter
     this.load.crossOrigin = "anonymous";
     this.load.image("Neo", NeoImg);
     this.load.image("caveTiles", caveTiles);
     this.load.tilemapTiledJSON("LVL1", LVL1);
-<<<<<<< HEAD
     this.load.tilemapTiledJSON("LVL2", LVL2)
-    this.load.image("BG1", BG1)
-    //this.load.image("BG2")
-=======
     this.load.image("BG1", BG1)
     this.load.image("BG2")
     this.load.image("energyBall", NeoImg);
     this.load.image("mask", mask)
->>>>>>> emitter
   }
 
   create() {
