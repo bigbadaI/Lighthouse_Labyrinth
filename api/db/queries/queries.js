93404f971b2c84c37d91d099c5661bd669959d26
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-03-18 16:30:13 -0700

    updated queries

--- api/db/queries/queries.js
+++ api/db/queries/queries.js
@@ -1,32 +1,48 @@
 // const db = require('../../lib/db.js'); db => pool
 
+const { query } = require("express");
+
 //adds new user, expects input = {id: ..., username: ...}
 const newUser = function (user) {
-  input = [user.id, user.name];
+  input = user.name;
   return db.query(`
     INSERT INTO users(id,username) 
-    VALUES($1,$2)
+    VALUES($1)
   `, input)
 };
 
-//add player_stats, expects input = {id: ..., user_id: ..., energy_score: ..., time_elapsed: ... }
+//add player_stats, expects input = {username: name, energy_score: ..., time_elapsed: ... }
 const newStats = function (stats) {
-  input = [stats.id, stats.user_id, stats.energy_score, stats.time_elapsed];
+  input = [stats.username, stats.energy_score, stats.time_elapsed];
   return db.query(`
-    INSERT INTO player_stats(id, user_id, energy_score, time_elasped)
-    VALUES($1, $2, $3, $4)
+    with grabUserId as (
+      SELECT id FROM users
+      WHERE username = $1
+    )
+    INSERT INTO player_stats(user_id, energy_score, time_elasped)
+    VALUES(grabUserId, $2, $3)
   `, input)
 };
 
-//update player_stats, updates energy and time if the scores are higher than previous held
-const updateStats = function (stats) {
-  const keys = ['id', 'user_id', 'energy_score', 'time_elapsed'];
-  const vals = keys.map(key => stats[key]); //undefined if not there
+const highScore = function () {
   return db.query(`
-    UPDATE player_stats
-    SET energy_score = coalesce($3, energy_score)
-    time_elapsed = coalesce($4, time_elapsed)
-    WHERE user_id = $2
-    RETURNING *;
-  `, vals)
+  SELECT player_stats.energy_score, player_stats.time_elapsed, users.username
+  FROM player_stats
+  JOIN users ON user_id = users.id
+  ORDER BY energy_score DESC
+  LIMIT 5
+  `)
 }
+
+// //update player_stats, updates energy and time if the scores are higher than previous held
+// const updateStats = function (stats) {
+//   const keys = ['id', 'user_id', 'energy_score', 'time_elapsed'];
+//   const vals = keys.map(key => stats[key]); //undefined if not there
+//   return db.query(`
+//     UPDATE player_stats
+//     SET energy_score = coalesce($3, energy_score)
+//     time_elapsed = coalesce($4, time_elapsed)
+//     WHERE user_id = $2
+//     RETURNING *;
+//   `, vals)
+// }
