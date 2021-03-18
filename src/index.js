372cb5ce2febd3263148c0e7b9e39b2ad3a97a15
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-03-18 16:09:42 -0700

    shaders

--- src/index.js
+++ src/index.js
@@ -10,92 +10,27 @@ import Preloader from "./phaser/helper/preloader";
 import InputPanel from "./phaser/inputpanel";
 import Starfield from './phaser/starfield';
 import Highscore from './phaser/highscores';
+import Neo from "./assets/Neo.png";
 
-
-
-export const config = {
-  // type: Phaser.AUTO,
+var config = {
   type: Phaser.WEBGL,
-  parent: "phaser",
   width: 600,
   height: 450,
+  parent: 'phaser',
   physics: {
     default: 'arcade',
     arcade: {
       gravity: 0,
       debug: false
-    }
+    },
   },
-
   scene: [Preloader, StartScene, LVL1, LVL2, LVL2B],
-  // Starfield, Highscore, InputPanel,
   scale: {
     zoom: 1.5
   }
 };
 
-const CustomPipeline = new Phaser.Class({
-
-  Extends: Phaser.Renderer.WebGL.Pipelines.SinglePipeline,
-
-  initialize:
-
-  function CustomPipeline (game)
-  {
-      Phaser.Renderer.WebGL.Pipelines.SinglePipeline.call(game, {
-          game: game,
-          fragShader: `
-          precision mediump float;
-
-          uniform sampler2D uMainSampler;
-          uniform vec2 uResolution;
-          uniform float uTime;
-
-          varying vec2 outTexCoord;
-          varying vec4 outTint;
-
-          vec4 plasma()
-          {
-              vec2 pixelPos = gl_FragCoord.xy / uResolution * 20.0;
-              float freq = 0.8;
-              float value =
-                  sin(uTime + pixelPos.x * freq) +
-                  sin(uTime + pixelPos.y * freq) +
-                  sin(uTime + (pixelPos.x + pixelPos.y) * freq) +
-                  cos(uTime + sqrt(length(pixelPos - 0.5)) * freq * 2.0);
-
-              return vec4(
-                  cos(value),
-                  sin(value),
-                  sin(value * 3.14 * 2.0),
-                  cos(value)
-              );
-          }
-
-          void main()
-          {
-              vec4 texture = texture2D(uMainSampler, outTexCoord);
-
-              texture *= vec4(outTint.rgb * outTint.a, outTint.a);
-
-              gl_FragColor = texture * plasma();
-          }
-          `,
-          uniforms: [
-              'uProjectionMatrix',
-              'uViewMatrix',
-              'uModelMatrix',
-              'uMainSampler',
-              'uResolution',
-              'uTime'
-          ]
-      });
-  }
-});
-
-const game = new Phaser.Game({ config, CustomPipeline });
-
-
+var game = new Phaser.Game(config);
 
 ReactDOM.render(
   <App />,
--- src/phaser/start_scene.js
+++ src/phaser/start_scene.js
@@ -2,32 +2,88 @@ import Phaser from "phaser";
 import BG from "../assets/backgrounds/start_scene.jpg";
 const gameState = {};
 
+var CustomPipeline = new Phaser.Class({
+
+  Extends: Phaser.Renderer.WebGL.Pipelines.SinglePipeline,
+
+  initialize:
+
+  function CustomPipeline (game)
+  {
+      Phaser.Renderer.WebGL.Pipelines.SinglePipeline.call(this, {
+          game: game,
+          fragShader: `
+          precision mediump float;
+          uniform float     time;
+          uniform vec2      resolution;
+          uniform vec2      mouse;
+
+          const float Tau        = 6.2832;
+          const float speed  = .02;
+          const float density    = .04;
+          const float shape  = .04;
+
+          float random( vec2 seed ) {
+            return fract(sin(seed.x+seed.y*1e3)*1e5);
+          }
+
+          float Cell(vec2 coord) {
+            vec2 cell = fract(coord) * vec2(.5,2.) - vec2(.0,.5);
+            return (1.-length(cell*2.-1.))*step(random(floor(coord)),density)*2.;
+          }
+          void main( void ) {
+            vec2 p = gl_FragCoord.xy / resolution  - mouse;
+
+            float a = fract(atan(p.x, p.y) / Tau);
+            float d = length(p);
+
+            vec2 coord = vec2(pow(d, shape), a)*256.;
+            vec2 delta = vec2(-time*speed*256., .5);
+            //vec2 delta = vec2(-time*speed*256., cos(length(p)*10.)*2e0+time*5e-1); // wavy wavy
+
+            float c = 0.;
+            for(int i=0; i<3; i++) {
+                coord += delta;
+                c = max(c, Cell(coord));
+            }
+
+            gl_FragColor = vec4(c*d);
+          }
+           `
+          // uniforms: [
+          //     'uProjectionMatrix',
+          //     'uViewMatrix',
+          //     'uModelMatrix',
+          //     'uMainSampler',
+          //     'uResolution',
+          //     'uTime'
+          // ]
+      });
+  }
+});
+
 export default class StartScene extends Phaser.Scene {
   constructor() {
     super({ key: "StartScene" });
   }
 
-  init(data){
-    console.log('init', data);
-    gameState.customPipeline = data.customPipeline;
-  }
+  // init(data){
+  //   console.log('init', data);
+  //   gameState.customPipeline = data.customPipeline;
+  // }
 
   preload() {
-    console.log("load");
     this.load.image("BG", BG);
   }
   create() {
-
-    gameState.customPipeline = this.renderer.pipelines.add('Custom', new CustomPipeline(this));
-    gameState.customPipeline.set2f('uResolution', config.width, config.height);
-
-    const sprite = this.add.sprite("BG").setPipeline('Custom');
-    sprite.width = 800;
-    sprite.height = 600;
+    // const sprite = this.add.sprite("BG1");
+    // sprite.width = 800;
+    // sprite.height = 600;
+    // gameState.time = 0;
+    this.customPipeline = this.renderer.pipelines.add('Custom', new CustomPipeline(this.game));
+    this.customPipeline.set2f('uResolution', this.game.config.width, this.game.config.height);
+    this.add.sprite(300, 200, "BG").setScale(2).setPipeline("Custom");
     
- 
-
-    // this.add.image(300, 200, "BG").setScale(2);
     this.add.text(200, 50, "Click to start!", {
       fill: "#ffffff",
       fontSize: "20px",
@@ -49,6 +105,7 @@ export default class StartScene extends Phaser.Scene {
   }
 
   update() {
-    // filter.update(this.input.activePointer);
+    // this.customPipeline.set1f('uTime', gameState.time);
+    // gameState.time += 0.05;
   }
 }
